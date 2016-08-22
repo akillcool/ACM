f36859017e470be8a1e7f511d1d4ca2c3d06c0fe
akillcool <akillcool@outlook.com>
2016-08-22 12:50:11 +0800

    //1325: 算法2 - 3~2 - 6：Big Bang

--- ACM/mergelist.cpp
+++ ACM/mergelist.cpp
@@ -1,210 +1,142 @@
-////1325: 算法2 - 3~2 - 6：Big Bang Example
+//1325: 算法2 - 3~2 - 6：Big Bang
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+#define INIT_SIZE 100
+#define INCREMENT 10
 
-typedef int SizeType;
+typedef struct
+{
+	char name[30];
+} Elem;
 
-#ifdef DEBUG
-#define debug printf
-// for memory leak detect.
-typedef struct MemBlock {
-	void* next;
-	long id;
-	long size;
-}MemBlock;
+typedef struct
+{
+	Elem *elem;
+	int length;
+	int listsize;
+} SqList;
 
-MemBlock gMallocFake = { 0, 0 };
+//初始化
+void InitList_Sq(SqList *L);
+//在L中的第i个元素之前插入e
+void ListInsert_Sq(SqList *L, int i, Elem e);
+//删除L中的第i个元素，返回其值e
+void ListDelete_Sq(SqList *L, Elem e);
+//查找L中第i个元素和e相同的值，返回其位置
+int LocateElem_Sq(SqList L, Elem e);
+//打印出表中所有元素
+void ShowElem_Sq(SqList L);
 
-long gIdCounter = 0;
-long gMallocCounter = 0;
+int main(void)
+{
+	SqList L;
+	InitList_Sq(&L);
 
-void* MallocWrapper(unsigned size) {
-	char* cp = NULL;
-	MemBlock* p = (MemBlock*)malloc(size + sizeof(MemBlock));
-	assert(p);
+	Elem e;
+	char str[30];
+	int num;
 
-	++gMallocCounter;
-	p->id = ++gIdCounter;
-	p->size = size;
-	p->next = gMallocFake.next;
-	gMallocFake.next = p;
-
-	cp = sizeof(MemBlock) + (char*)p;
-	debug("Malloc %d bytes at %p, id: %ld, counter: %ld\n", size, cp, p->id, gMallocCounter);
-	return cp;
-}
-
-void FreeWrapper(void* mem) {
-	MemBlock* p = gMallocFake.next;
-	MemBlock* prev = &gMallocFake;
-	while (p) {
-		if (mem == sizeof(MemBlock) + (char*)p) {
-			debug("Free %ld bytes at %p, id: %ld, counter: %ld\n", p->size, mem, p->id, gMallocCounter);
-			--gMallocCounter;
-			prev->next = p->next;
-			free(p);
-			return;
+	while (scanf("%s", str) == 1)
+	{
+		if (!strcmp(str, "insert"))
+		{
+			scanf("%d%s", &num, e.name);
+			ListInsert_Sq(&L, num, e);
 		}
-		prev = p;
-		p = p->next;
-	}
-	debug("Untracked block %p\n", mem);
-}
-
-char* Strdup(char* str) {
-	int len = strlen(str) + 1;
-	char* dst = (char*)MallocWrapper(len * sizeof(char));
-	strcpy(dst, str);
-	return dst;
-}
-
-#define malloc MallocWrapper
-#define free   FreeWrapper
-#define strdup Strdup
-#else // DEBUG
-void nop(char* fmt, ...) {}
-#define debug nop
-#endif // DEBUG
-
-// begin ItemType:
-typedef char* ItemType;
-
-void ItemPrint(ItemType item) {
-	printf("%s ", item);
-}
-
-int ItemMatch(ItemType a, ItemType b) {
-	return !strcmp(a, b);
-}
-
-void ItemDestroy(ItemType item) {
-	debug("destroy %p %s\n", item, item);
-	free(item);
-}
-// end ItemType
-
-typedef struct ArrayListSt {
-	ItemType* data;
-	SizeType  size;
-	SizeType  capacity;
-}ArrayList;
-
-void ArrayListInfo(ArrayList* list) {
-	printf("list: %p, size: %d, capacity: %d\n", list, list->size, list->capacity);
-}
-
-ArrayList* ArrayListCreate(int hint) {
-	ArrayList* list = (ArrayList*)malloc(sizeof(ArrayList));
-	assert(list);
-	list->size = 0;
-
-	int capacity = 16;
-	while (capacity < hint) capacity *= 2;
-	list->capacity = capacity;
-
-	list->data = (ItemType*)malloc(capacity * sizeof(ItemType));
-	assert(list->data);
-
-	return list;
-}
-
-#define ArrayListForeach(list, visitor) do { \
-    int i; \
-    for(i = 0; i < list->size; ++i) { \
-        visitor(list->data[i]); \
-    } \
-}while(0)
-
-void ArrayListDestroy(ArrayList* list) {
-	ArrayListForeach(list, ItemDestroy);
-	free(list->data);
-	free(list);
-}
-
-void ArrayListInsert(ArrayList* list, SizeType num, ItemType val) {
-	int idx = num - 1;
-	assert(0 <= idx && idx <= list->size);
-
-	debug(">insert %d %s\n", num, val);
-	if (idx < list->size) {
-		int i;
-		for (i = list->size - 1; i >= idx; --i) {
-			list->data[i + 1] = list->data[i];
+		else if (!strcmp(str, "delete"))
+		{
+			scanf("%s", e.name);
+			ListDelete_Sq(&L, e);
+		}
+		else if (!strcmp(str, "search"))
+		{
+			scanf("%s", e.name);
+			printf("%d\n", LocateElem_Sq(L, e));
+		}
+		else if (!strcmp(str, "show"))
+		{
+			ShowElem_Sq(L);
 		}
 	}
-
-	list->data[idx] = val;
-	list->size++;
-	// ArrayListForeach(list, ItemPrint); printf("\n\n");
+	return 0;
 }
 
-void ArrayListDelete(ArrayList* list, ItemType val) {
-	int i, target = -1;
+void InitList_Sq(SqList *L)
+{
+	(*L).elem = (Elem*)malloc(INIT_SIZE * sizeof(Elem));
+	if (!(*L).elem)
+		exit(EXIT_FAILURE);
+	(*L).length = 0;
+	(*L).listsize = INIT_SIZE;
+}
 
-	debug(">delete %s\n", val);
-	for (i = 0; i < list->size; ++i) {
-		if (ItemMatch(list->data[i], val)) {
-			target = i;
-			ItemDestroy(list->data[i]);
-			break;
+void ListInsert_Sq(SqList *L, int i, Elem e)
+{
+	if (i<1 || i>(*L).length + 1)
+		exit(EXIT_FAILURE);
+
+	Elem *p, *q;
+
+	if ((*L).length >= (*L).listsize)
+	{
+		Elem *newbase;
+
+		newbase = (Elem *)realloc((*L).elem, ((*L).listsize + INCREMENT) * sizeof(Elem));
+		if (!newbase)
+			exit(EXIT_FAILURE);
+		(*L).elem = newbase;
+		(*L).listsize += INCREMENT;
+	}
+	q = (*L).elem + i - 1; //q为插入位置
+	for (p = (*L).elem + (*L).length - 1; p >= q; --p)
+		*(p + 1) = *p;
+	*q = e;
+	++(*L).length;
+}
+
+void ListDelete_Sq(SqList *L, Elem e)
+{
+	int i = LocateElem_Sq(*L,e);
+	Elem *p,*q;	//q指向被删除的元素
+
+	q = (*L).elem + i - 1;
+	for (p = q; p < (*L).elem + (*L).length; ++p)
+	{
+		*p = *(p + 1);
+	}
+	--(*L).length;
+}
+
+int LocateElem_Sq(SqList L, Elem e)
+{
+	int count = 0;
+	Elem *p = L.elem;
+
+	while (p < L.elem + L.length)
+	{
+		++count;
+		if (!strcmp((*p).name, e.name))
+		{
+			return count;
 		}
+		++p;
 	}
-
-	if (target < 0) return;
-
-	for (i = target + 1; i < list->size; ++i) {
-		list->data[i - 1] = list->data[i];
-	}
-	list->size--;
-	// ArrayListForeach(list, ItemPrint); printf("\n\n");
+	return 0;
 }
 
-int ArrayListSearch(ArrayList* list, ItemType val) {
+void ShowElem_Sq(SqList L)
+{
 	int i;
 
-	debug(">search %s\n", val);
-	for (i = 0; i < list->size; ++i) {
-		if (ItemMatch(list->data[i], val)) return i + 1;
+	for (i = 0; i < L.length; i++)
+	{
+		if (i)
+		{
+			putchar(' ');
+		}
+		printf("%s", (L.elem[i].name));
 	}
-	return 0;
-}
-
-int main()
-{
-	char buffer[32]; // 每个名字不超过30个字符。
-
-	ArrayList *list = ArrayListCreate(10);
-	assert(list);
-
-	while (scanf("%s", buffer) == 1) {
-		if (!strcmp(buffer, "insert")) {
-			int num = 0;
-			scanf("%d%s", &num, buffer);
-			ArrayListInsert(list, num, strdup(buffer));
-		}
-		else if (!strcmp(buffer, "delete")) {
-			scanf("%s", buffer);
-			ArrayListDelete(list, buffer);
-		}
-		else if (!strcmp(buffer, "show")) {
-			ArrayListForeach(list, ItemPrint);
-			printf("\n");
-		}
-		else if (!strcmp(buffer, "search")) {
-			scanf("%s", buffer);
-			int num = ArrayListSearch(list, buffer);
-			printf("%d\n", num);
-		}
-		else {
-			// unknow.
-		}
-	}
-
-	// ArrayListForeach(list, ItemPrint); printf("\n");
-	ArrayListDestroy(list);
-
-	return 0;
+	putchar('\n');
 }
